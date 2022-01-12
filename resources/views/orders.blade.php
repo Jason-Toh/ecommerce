7bdaf42401324c902ecd9b51eb04880ef93ea4e0
Jason <tohjason28@gmail.com>
2022-01-12 12:14:36 +0800

    edited order view file

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,13 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($orderItems as $orderItem)
-        <?php
-            $order = $orderItem['order'];
-            $products = $orderItem['products'];
-        ?>
-        @foreach($products as $product)
-            {{$product['name']}}
-        @endforeach
-    @endforeach
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col" class="text-center">No.</th>
+                <th scope="col" class="text-center">Date</th>
+                <th scope="col" class="text-center">Items</th>
+                <th scope="col" class="text-center">Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $count = 0; ?>
+            @foreach($orderItems as $orderItem)
+                <tr>
+                    <?php
+                        $count += 1;
+                        $order = $orderItem['order'];
+                        $products = $orderItem['products'];
+                    ?>
+                    <td class="text-center">{{ $count }}</td>
+                    <td class="text-center">{{ $order['created_at'] }}</td>
+                    <td>
+                        @foreach($products as $product)
+                            <img src="{{ $product['image'] }}" class="img-fluid rounded"/>
+                            <div>
+                                <div>
+                                    {{ $product['name'] }}
+                                </div>
+                                <div>
+                                    {{ $product['description'] }}
+                                </div>
+                            </div>
+                        @endforeach
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
 @endsection
